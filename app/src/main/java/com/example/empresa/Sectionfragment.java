26795d9c7b56f83940c130fe7fa7377ecceaab2f
505 <505@academicos.cesde.edu.co>
2022-09-16 19:43:56 -0500

    clase 16-09-2022

--- app/src/main/java/com/example/empresa/Sectionfragment.java
+++ app/src/main/java/com/example/empresa/Sectionfragment.java
@@ -10,10 +10,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
 
 import org.json.JSONObject;
@@ -38,18 +41,42 @@ public class Sectionfragment extends Fragment implements Response.Listener<JSONO
         jetclave = vista.findViewById(R.id.etclave);
         jbtingresar = vista.findViewById(R.id.btingresar);
         jtvregistrar=vista.findViewById(R.id.tvregistrar);
-        jbtingresar.setOnClickListener();
+        jbtingresar.setOnClickListener(new  View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+
+
+        });
         return vista;
+
+
+    }
+
+    public void  Iniciar_sesion(){
+        String correo,clave;
+        correo=jetcorreo.getText().toString();
+        clave=jetclave.getText().toString();
+        if (correo.isEmpty() || clave.isEmpty()){
+            Toast.makeText(getContext(), "correo y clave son requeridos", Toast.LENGTH_SHORT).show();
+            jetcorreo.requestFocus();
+        }
+        else {
+            String url = "http://172.16.58.58/WebServices/Sesion.php/sesion.php?correo=" + correo + "&clave=" + clave;
+            jrq = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
+            rq.add(jrq);
+        }
 
     }
 
     @Override
     public void onErrorResponse(VolleyError error) {
-
+        Toast.makeText(getContext(), "error en inicio de sesion", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onResponse(JSONObject response) {
-
+        Toast.makeText(getContext(), "mela la vuelta", Toast.LENGTH_SHORT).show();
     }
 }
